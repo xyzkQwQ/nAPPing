a638ea50f689e97723ffb25e6863b3e14d726eb1
¿¡ <maxii.larranaga@gmail.com>
2026-06-23 11:29:20 +0200

    Ajout d'une variable d'environnement VITE_BASE_URL pour l'environnement de production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+// export default defineConfig({
+//   base: env.VITE_BASE_URL || '/',
+//   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+// });
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".");
+
+  return {
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+    test: {
+      environment: "node",
+      globals: true,
+    },
+    base: env.VITE_BASE_URL || "/",
+  };
 });
